54a1e7260f72dc84525b095353867f159270a9f2
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2016-04-17 19:20:34 +1200

    Added app example.

---
+++ Set-1/Challenge-1/app.js
@@ -0,0 +1,3 @@
+var converter = require('./hex2base64');
+
+console.log(converter.convertHexToBase64('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'))
