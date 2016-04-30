b5893a0afa7107839fa748c4d9ef0c9f4a0d12ff
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2016-04-30 16:24:12 +1200

    Rewrote using FindTheBiggest algorithm.

--- Set-01/Challenge-04/singleCharacterXORDetector.js
+++ Set-01/Challenge-04/singleCharacterXORDetector.js
@@ -2,11 +2,21 @@
     var findSingleByteXORKey = require('./findSingleByteXORKey').find;
     var singleByteXOR = require('./singleByteXOR');
 
+    
     var detectSingleCharXOR = function(ciphertexts){
-        return ciphertexts.map(function(ciphertext){
-            var messages = singleByteXOR.decrypt(ciphertext);
-            return findSingleByteXORKey(messages).message
-        })
+        var winner = {
+            'ascii': 0,
+            'spaces': 0
+        };
+
+
+        for(var i = 0; i < ciphertexts.length; i++){
+            var bestOfSet = findSingleByteXORKey(singleByteXOR.decrypt(ciphertexts[i]));
+            if(winner.ascii < bestOfSet.ascii && bestOfSet.spaces)
+                winner = bestOfSet;
+        }
+
+        return winner;
     }
 
     exports.detect = detectSingleCharXOR;
